bc067cb1d2d1a64fd4d075004b4b839de163c18d
SarangVThakare <sarangvthakare1@gmail.com>
2024-10-14 12:06:28 +0530

    Update X.cpp

--- X.cpp
+++ X.cpp
@@ -1,10 +1,25 @@
 #include<iostream>
-#include<string>
+#include<cstring>
 using namespace std;
 
 int main(){
-string name;
-ci
+char[10] name;
+cin>>name;
+char names[4][10]={"bruise","tony","peter"};
+int n=4;
+  arr[4]=name;
+  for(int i=0;i<n;i++){
+for(int j=i;j<n;j++){
+if(strcmp(names[i],names[j])<=0){
+char [] temp=names[i];
+  names[i]=names[j];
+  names[j]=temp;
+}
+}
+for(int i=0;i<n;i++){
+cout<<names[i]<<endl;
+}
+}
 
 return 0;
 }
